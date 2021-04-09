bbb42c70769e1f79f7e53aa686dbcce8124161ce
Patricia Carla Soares <patriciacarlasoares98@gmail.com>
2021-04-09 06:04:28 -0300

    Correção do erro de conexão
    ConexaoDao: Unknow database 'oliveira-trade/'

--- JavaApplication72/src/Dao/ConexaoBD.java
+++ JavaApplication72/src/Dao/ConexaoBD.java
@@ -8,10 +8,10 @@ public class ConexaoBD {
     public Connection getConnetion() {
         Connection conexao = null;
         try {
-            String url= "jdbc:mysql://localhost:3306/oliveira_trade/?user=root&password=";
+            String url= "jdbc:mysql://localhost:3306/oliveira_trade?user=root&password=";
             conexao = DriverManager.getConnection(url);
         } catch (Exception erro) {
-            JOptionPane.showMessageDialog(null,erro.getMessage());
+            JOptionPane.showMessageDialog(null,"ConexaoDao: "+erro.getMessage());
         }
         return conexao;
     }
